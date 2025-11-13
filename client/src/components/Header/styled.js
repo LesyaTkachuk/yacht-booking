34eb69d43c310a964e7c6ab70ff69d6dae5cc588
LesyaTkachuk <lesyalesya9876@gmail.com>
2025-11-14 01:26:46 +0200

    adjust recommendation model and test dif algos and params

--- client/src/components/Header/styled.js
+++ client/src/components/Header/styled.js
@@ -3,6 +3,9 @@ import { styled, css } from "@mui/material/styles";
 
 export const StyledStack = styled(Stack)(({ theme }) =>
   css({
+    position: "sticky",
+    top: 0,
+    zIndex: 100,
     backgroundColor: theme.palette.special.white,
     padding: "24px 62px",
     justifyContent: "space-between",
--- client/src/components/PageWrapper/styled.js
+++ client/src/components/PageWrapper/styled.js
@@ -2,5 +2,6 @@ import { styled, css } from "@mui/material/styles";
 import { Stack } from "@mui/material";
 
 export const StyledWrapper = styled(Stack)`
+  position: relative;
   min-height: calc(100vh - 98px);
 `;
